4f0d0a33e5601ccc90e05f6d7d9a7f6275e241d2
NatPhoto <nataliasanc@gmail.com>
2015-09-23 20:49:03 +0100

    update to assignment 2 functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This R file consists of two functions, makeCacheMatrix and cacheSolve.  The first creates 
+## a special "matrix" object that can cache its inverse. Then the cacheSolve computes the inverse of
+## the special "matrix".
 
-## Write a short comment describing this function
+## makeCacheMatrix is a funciton that stores a list of functions set, get, setINV, and getINV. No
+## calculation of the inverse is performed.  
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(x=matrix()) {
+  
+  #set, get, setINV, and getINV are stored as objects in a list.  These will be used as inputs in
+  #cacheSolve().
+  
+  m=NULL
+  set=function(y) {
+    x<<-y
+    m<<-NULL
+  }
+  get=function() x
+  setINV=function(Inver) m<<-Inver         
+  getINV=function() m
+  list(set=set, get=get, setINV=setINV, getINV=getINV)
 }
 
+## cacheSolve will evalute the input and compare it against the cached value stored from makeCacheMatrix
+## and if not found it will take the inverse of the matrix.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(x,...) { 
+  m=x$getINV()
+  
+  #If the inverse is stored in cache it will get it will print the message and return the value
+  #and skips the calculation.
+  
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  #this will calculate the inverse of the matrix if the above is false.
+  
+  data=x$get()
+  m=solve(data,...)
+  x$setINV(m)
+  return(m)
 }
